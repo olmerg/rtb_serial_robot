0e1a385ea7b8d13fc0bc57f1222c897901e3d86f
unknown <jonathana.sechaguag@utadeo.edu.co>
2021-04-29 12:53:45 -0500

    prueba 2

---
+++ RobotUtadeo/urdf/RobotUtadeo.py
@@ -0,0 +1,32 @@
+import numpy as np
+from roboticstoolbox.robot.ERobot import ERobot
+from math import pi
+
+
+class RobotUtadeo(ERobot):
+    """
+    Class that imports a RobotUtadeo 
+    """
+
+    def __init__(self):
+
+        args = super().urdf_to_ets_args(
+            "lesson_urdf/urdf/planar_3dof.urdf")
+        
+        super().__init__(
+            args[0],
+            name=args[1])
+
+        self.manufacturer = "Utadeo"
+        # self.ee_link = self.ets[9]
+
+        # zero angles, L shaped pose
+        self.addconfiguration("qz", np.array([0, 0, 0, 0, 0]))
+
+        # ready pose, arm up
+        self.addconfiguration("qr", np.array([0, pi/2, pi/2, pi/2, pi/2]))
+
+if __name__ == '__main__':   # pragma nocover
+
+    Robot = RobotUtadeo()
+    print(Robot)

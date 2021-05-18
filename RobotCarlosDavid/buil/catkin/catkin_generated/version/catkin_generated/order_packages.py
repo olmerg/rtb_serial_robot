6e90c747eb8e546b1c1345733af13481d2adb583
DavidElRojas <80717892+DavidElRojas@users.noreply.github.com>
2021-05-18 09:48:47 -0500

    Add files via upload

---
+++ RobotCarlosDavid/buil/catkin/catkin_generated/version/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/myrobot/Robotatto/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/myrobot/Robotatto/devel;/opt/ros/melodic".split(';') if "/home/myrobot/Robotatto/devel;/opt/ros/melodic" != "" else []
